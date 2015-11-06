0663741ab9a9f93c2d6c2f74aace992e6cb3249b
Lautaro Gonzalez <lautaro.ngc@gmail.com>
2015-11-06 15:12:31 -0300

    user piano

--- Userland/SampleCodeModule/sounds.c
+++ Userland/SampleCodeModule/sounds.c
@@ -16,10 +16,21 @@ void piano(){
 	print_message("|  | | | |  |  | | | | | |  |  | | | |  |\n",0xFF);
 	print_message("|  | | | |  |  | | | | | |  |  | | | |  |\n",0xFF);
 	print_message("|  | | | |  |  | | | | | |  |  | | | |  |\n",0xFF);
-	print_message("|  | | | |  |  | | | | | |  |  | | | |  |\n",0xFF);
+	print_message("|  |2| |3|  |  |5| |6| |7|  |  |9| | |  |\n",0xFF);
 	print_message("|  |_| |_|  |  |_| |_| |_|  |  |_| |_|  |\n",0xFF);
 	print_message("|   |   |   |   |   |   |   |   |   |   |\n",0xFF);
+	print_message("| Q | W | E | R | T | Y | U | I |   |   |\n",0xFF);
+	print_message("|___|___|___|___|___|___|___|___|___|___|\n\n ",0xFF);
+	
+	
+	print_message(" _______________________________________ \n",0xFF);
+	print_message("|  | | | |  |  | | | | | |  |  | | | |  |\n",0xFF);
+	print_message("|  | | | |  |  | | | | | |  |  | | | |  |\n",0xFF);
+	print_message("|  | | | |  |  | | | | | |  |  | | | |  |\n",0xFF);
+	print_message("|  | | |S|  |  |F| |G| |H|  |  | | | |  |\n",0xFF);
+	print_message("|  |_| |_|  |  |_| |_| |_|  |  |_| |_|  |\n",0xFF);
 	print_message("|   |   |   |   |   |   |   |   |   |   |\n",0xFF);
+	print_message("|   | Z | X | C | V | B | N | M |   |   |\n",0xFF);
 	print_message("|___|___|___|___|___|___|___|___|___|___|\n\n ",0xFF);
 	
 	_call_int80(INT_PIANO);
