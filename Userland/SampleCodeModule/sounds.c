93ea6ac163db427931219d780de510d2b910f048
natinavas <nnavas@itba.edu.ar>
2015-11-09 23:31:21 -0300

    ascii art por doquier

--- Userland/SampleCodeModule/sounds.c
+++ Userland/SampleCodeModule/sounds.c
@@ -1,57 +1,72 @@
-#include "sounds.h"
-#include "lib.h"
+	#include "sounds.h"
+	#include "lib.h"
 
-void beep(){
-	_call_int80(INT_BEEP);
-}
+	void beep(){
+		_call_int80(INT_BEEP);
+	}
 
-void songs(){
-	int flag = 1;
-	print_message("Choose the number of the masterpiece you wish to hear: \n",0xFF);
-	print_message("1. Because - The Bits\n", 0xFF);
-	print_message("2. Piano Concerto No. 5 - Beethoven\n", 0xFF);
-	print_message("3. song3\n", 0xFF);
-	print_message("4. song4\n", 0xFF);
-	char aux[10];
+	void songs(){
+		int flag = 1;
 	
-	while(flag){
-		readKeyboard(aux);
-		uint64_t value = atoi(aux); 
-		if(value >= 0 && value <=4){
-			flag = 0;
-			_call_int80(INT_SONGS, value);
-		}else{
-			print_message("Please enter a valid number\n", 0xFF);
+		shell_erase_screen();
+		print_message("\n ___|)________________  _____ _____ _____ _____ _____  __________\n",0xFF);
+		print_message("|___/_________________ |   __|     |   | |   __|   __| __________\n",0xFF);
+		print_message("|__/|_________________ |__   |  |  | | | |  |  |__   | __________\n",0xFF);
+		print_message("|_/(|,\\_______________ |_____|_____|_|___|_____|_____| __________\n",0xFF);
+		print_message("|_\\_|_/__________________________________________________________\n",0xFF);
+		print_message("|   |  \n",0xFF);
+		print_message("| (_|  \n\n",0xFF);
+	
+		print_message("Choose the number of the masterpiece you wish to hear: \n",0xFF);
+		print_message("  1. Because - The Bits\n", 0xFF);
+		print_message("  2. Piano Concerto No. 5 - Beethoven\n", 0xFF);
+		print_message("  3. song3\n", 0xFF);
+		print_message("  4. song4\n", 0xFF);
+		print_message("  5. exit play songs :( \n\n", 0xFF);
+		char aux[10];
+	
+		while(flag){
+			readKeyboard(aux);
+			uint64_t value = atoi(aux); 
+			if(value >= 0 && value <=4){
+				flag = 0;
+				_call_int80(INT_SONGS, value);
+			}else if(value == 5){
+				flag = 0;
+			}else{
+				print_message("Please enter a valid number\n", 0xFF);
+			}
 		}
 	}
-}
 	
 
-	void piano(){
+		void piano(){
 	
-		print_message(" _______________________________________ \n",0xFF);
-		print_message("|  | | | |  |  | | | | | |  |  | | | |  |\n",0xFF);
-		print_message("|  | | | |  |  | | | | | |  |  | | | |  |\n",0xFF);
-		print_message("|  | | | |  |  | | | | | |  |  | | | |  |\n",0xFF);
-		print_message("|  |2| |3|  |  |5| |6| |7|  |  |9| | |  |\n",0xFF);
-		print_message("|  |_| |_|  |  |_| |_| |_|  |  |_| |_|  |\n",0xFF);
-		print_message("|   |   |   |   |   |   |   |   |   |   |\n",0xFF);
-		print_message("| Q | W | E | R | T | Y | U | I |   |   |\n",0xFF);
-		print_message("|___|___|___|___|___|___|___|___|___|___|\n\n ",0xFF);
+			shell_erase_screen();
+		
+			print_message(" _______________________________________ \n",0xFF);
+			print_message("|  | | | |  |  | | | | | |  |  | | | |  |\n",0xFF);
+			print_message("|  | | | |  |  | | | | | |  |  | | | |  |\n",0xFF);
+			print_message("|  | | | |  |  | | | | | |  |  | | | |  |\n",0xFF);
+			print_message("|  |2| |3|  |  |5| |6| |7|  |  |9| | |  |\n",0xFF);
+			print_message("|  |_| |_|  |  |_| |_| |_|  |  |_| |_|  |\n",0xFF);
+			print_message("|   |   |   |   |   |   |   |   |   |   |\n",0xFF);
+			print_message("| Q | W | E | R | T | Y | U | I |   |   |\n",0xFF);
+			print_message("|___|___|___|___|___|___|___|___|___|___|\n\n ",0xFF);
 	
 	
-		print_message(" _______________________________________ \n",0xFF);
-		print_message("|  | | | |  |  | | | | | |  |  | | | |  |\n",0xFF);
-		print_message("|  | | | |  |  | | | | | |  |  | | | |  |\n",0xFF);
-		print_message("|  | | | |  |  | | | | | |  |  | | | |  |\n",0xFF);
-		print_message("|  | | |S|  |  |F| |G| |H|  |  | | | |  |\n",0xFF);
-		print_message("|  |_| |_|  |  |_| |_| |_|  |  |_| |_|  |\n",0xFF);
-		print_message("|   |   |   |   |   |   |   |   |   |   |\n",0xFF);
-		print_message("|   | Z | X | C | V | B | N | M |   |   |\n",0xFF);
-		print_message("|___|___|___|___|___|___|___|___|___|___|\n\n ",0xFF);
+			print_message(" _______________________________________ \n",0xFF);
+			print_message("|  | | | |  |  | | | | | |  |  | | | |  |\n",0xFF);
+			print_message("|  | | | |  |  | | | | | |  |  | | | |  |\n",0xFF);
+			print_message("|  | | | |  |  | | | | | |  |  | | | |  |\n",0xFF);
+			print_message("|  | | |S|  |  |F| |G| |H|  |  | | | |  |\n",0xFF);
+			print_message("|  |_| |_|  |  |_| |_| |_|  |  |_| |_|  |\n",0xFF);
+			print_message("|   |   |   |   |   |   |   |   |   |   |\n",0xFF);
+			print_message("|   | Z | X | C | V | B | N | M |   |   |\n",0xFF);
+			print_message("|___|___|___|___|___|___|___|___|___|___|\n",0xFF);
 
-		print_message("\nPress ESC to leave the piano\n",0xFF);
+			print_message("\nPress ESC to leave the piano\n\n",0xFF);
 	
-		_call_int80(INT_PIANO);
+			_call_int80(INT_PIANO);
 	
-	}
+		}
